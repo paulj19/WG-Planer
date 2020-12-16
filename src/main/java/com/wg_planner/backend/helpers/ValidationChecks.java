095078440cc5963f4706befacb8e87a300c5a66f
d.paul <s8dipaul@stud.uni-saarland.de>
2020-12-16 17:32:57 +0100

    check if list is null before traversing

--- src/main/java/com/wg_planner/backend/helpers/ValidationChecks.java
+++ src/main/java/com/wg_planner/backend/helpers/ValidationChecks.java
@@ -1,10 +1,18 @@
 package com.wg_planner.backend.helpers;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ValidationChecks {
     public static class StringChecks {
-        public static boolean isAlphaNumeric(String s){
-            String pattern= "^[a-zA-Z0-9]*$";
+        public static boolean isAlphaNumeric(String s) {
+            String pattern = "^[a-zA-Z0-9]*$";
             return s.matches(pattern);
         }
+
+    }
+
+    public static <T extends Object> List<T> safe(List<T> other) {
+        return other == null ? Collections.EMPTY_LIST : other;
     }
 }
