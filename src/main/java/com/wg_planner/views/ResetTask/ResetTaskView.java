398df025bfe1f17e5c21375316af8dc3f76f06ec
d.paul <s8dipaul@stud.uni-saarland.de>
2021-01-26 20:00:17 +0100

    feat:reset_task view implementation and saving task

--- src/main/java/com/wg_planner/views/ResetTask/ResetTaskView.java
+++ src/main/java/com/wg_planner/views/ResetTask/ResetTaskView.java
@@ -1,19 +1,25 @@
 package com.wg_planner.views.ResetTask;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.*;
+import com.wg_planner.backend.Service.FloorService;
 import com.wg_planner.backend.Service.TaskService;
 import com.wg_planner.backend.entity.Room;
 import com.wg_planner.backend.entity.Task;
+import com.wg_planner.views.MyTasks.MyTasksView;
 import com.wg_planner.views.Tasks.TasksPresenter;
 import com.wg_planner.views.main.MainView;
+import com.wg_planner.views.signup.RegisterForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.logging.Level;
 
 @Route(value = "reset_task", layout = MainView.class)
@@ -23,13 +29,12 @@ import java.util.logging.Level;
 public class ResetTaskView extends VerticalLayout implements HasUrlParameter<String> {
     private Task taskToReset;
     private Room resetRequestingRoom;
-    H1 heading = new H1("Assign Biomüll to another roommate");
-    Label label = new Label("manchester united");
-    TextField firstName = new TextField("First Name", "please enter your first name");
-
 
     @Autowired
     TaskService taskService;
+    @Autowired
+    FloorService floorService;
+
     protected static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(ResetTaskView.class
             .getName());
 
@@ -38,6 +43,7 @@ public class ResetTaskView extends VerticalLayout implements HasUrlParameter<Str
                              @OptionalParameter String parameter) {
         if (parameter != null && !parameter.isEmpty()) {
             taskToReset = taskService.getTaskById(Long.parseLong(parameter));
+            resetRequestingRoom = taskToReset.getAssignedRoom();
             addResetPage();
         } else {
             LOGGER.log(Level.SEVERE, "url parameter in ResetTaskView must not be null or empty");
@@ -45,7 +51,21 @@ public class ResetTaskView extends VerticalLayout implements HasUrlParameter<Str
     }
 
     public void addResetPage() {
-        ResetTaskPage resetTaskPage = new ResetTaskPage();
+        ResetTaskPage resetTaskPage = new ResetTaskPage(taskToReset, resetRequestingRoom, floorService);
+        resetTaskPage.addListener(ResetTaskPage.ResetTaskPageEvent.ResetEvent.class, this::resetTask);
+        resetTaskPage.addListener(ResetTaskPage.ResetTaskPageEvent.CancelEvent.class, this::backToMyTasks);
         add(resetTaskPage);
     }
+
+    private void resetTask(ResetTaskPage.ResetTaskPageEvent.ResetEvent event)
+    {
+        taskService.resetTask(taskToReset, event.getRoomSelected());
+        UI.getCurrent().navigate(MyTasksView.class);
+    }
+
+    private void backToMyTasks(ResetTaskPage.ResetTaskPageEvent.CancelEvent event)
+    {
+        UI.getCurrent().navigate(MyTasksView.class);
+    }
+
 }
