0ccc769b97eb02b636a3f3e2a7bacdda732f4d8d
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-14 19:37:18 +0200

    floorCode generator test

--- src/test/java/com/wg_planner/backend/entity/FloorCodeGenerationTest.java
+++ src/test/java/com/wg_planner/backend/entity/FloorCodeGenerationTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.stream.Stream;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 //@ActiveProfiles("test")
-//@Transactional
+@Transactional
 public class FloorCodeGenerationTest {
     @Autowired
     private FloorService floorService;
