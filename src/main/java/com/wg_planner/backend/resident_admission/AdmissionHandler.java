ddbc9a43b356ccb3fa1351e8fe984a468f1dd110
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-15 20:59:52 +0200

    generateAndSaveAdmissionCode

--- src/main/java/com/wg_planner/backend/resident_admission/AdmissionHandler.java
+++ src/main/java/com/wg_planner/backend/resident_admission/AdmissionHandler.java
@@ -1,4 +1,47 @@
 package com.wg_planner.backend.resident_admission;
 
+import com.wg_planner.backend.Service.FloorService;
+import com.wg_planner.backend.entity.Floor;
+import com.wg_planner.backend.entity.Room;
+import com.wg_planner.backend.utils.code_generator.custom_code_generator.CustomCodeCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+@Controller
 public class AdmissionHandler {
+    private FloorService floorService;
+    private AdmissionCodeStore admissionCodeStore;
+
+    @Autowired
+    public AdmissionHandler(FloorService floorService, AdmissionCodeStore admissionCodeStore) {
+        this.floorService = floorService;
+        this.admissionCodeStore = admissionCodeStore;
+    }
+
+    public List<Room> verifyFloorCodeAndGetVacantRoomsInFloor(String floorCode) {
+        Floor floor = floorService.getFloorByFloorCode(floorCode);
+        if (floor == null) {
+            return null;
+        }
+        return floorService.getAllNonOccupiedRoomsInFloor(floor);
+    }
+
+    public synchronized AdmissionCode generateAndSaveAdmissionCode(Room roomToAdmit) {//synced to ensure unique admission codes
+        Assert.notNull(roomToAdmit, "room to admit must not be null");
+        Assert.isTrue(roomToAdmit.isOccupied() == false, "selected room isOccupied value must be false");
+        Assert.isTrue(roomToAdmit.getResidentAccount() == null, "selected room residentAccount value must be null");
+
+        AdmissionDetails admissionDetails = new AdmissionDetails(roomToAdmit);
+        AdmissionCode admissionCode;
+        do {
+            admissionCode = new AdmissionCode(CustomCodeCreator.getInstance().generateCode(CustomCodeCreator.CodeGenerationPurposes.ADMISSION_CODE));
+        } while (admissionCodeStore.containsAdmissionCode(admissionCode));
+        if (admissionCodeStore.saveAdmissionCode(admissionCode, admissionDetails).equals(admissionDetails)) {
+            return admissionCode;
+        }
+        return null;
+    }
 }
