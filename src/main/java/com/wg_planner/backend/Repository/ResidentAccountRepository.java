6b71e9fa2c3d465253309bff9400eac131efb0ce
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-16 21:40:21 +0200

    using active field in query to eliminate deleted accounts

--- src/main/java/com/wg_planner/backend/Repository/ResidentAccountRepository.java
+++ src/main/java/com/wg_planner/backend/Repository/ResidentAccountRepository.java
@@ -1,19 +1,13 @@
 package com.wg_planner.backend.Repository;
 
-import com.wg_planner.backend.entity.Account;
 import com.wg_planner.backend.entity.ResidentAccount;
-import com.wg_planner.backend.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ResidentAccountRepository extends JpaRepository<ResidentAccount, Long> {
-    //TODO include accId in the query
-    @Query("select ra from ResidentAccount ra where ra.id = :accountId ")
+    @Query("select ra from ResidentAccount ra where ra.id = :accountId and ra.active = true")
     ResidentAccount getResidentAccount(Long accountId);
 
-//    @Query(value = "select ra from ResidentAccount ra where ra.room.id = :roomId ", nativeQuery = true)
-    @Query(value = "select ra from ResidentAccount ra where ra.room.id = :roomId ")
+    @Query(value = "select ra from ResidentAccount ra where ra.room.id = :roomId and ra.active = true")
     ResidentAccount getResidentAccountByRoom(Long roomId);
-//    @Query(value = "select ra from Account ra where ra.userName LIKE :username")
-//    ResidentAccount getResidentAccountByUsername(String username);
 }
