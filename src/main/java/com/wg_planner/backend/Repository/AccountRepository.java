cfb221bd46e09bf79d5f92610ddfc5ec792bea51
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-26 18:58:06 +0200

    AccountRepository active in query

--- src/main/java/com/wg_planner/backend/Repository/AccountRepository.java
+++ src/main/java/com/wg_planner/backend/Repository/AccountRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    @Query("select a from Account a where a.username like :username")
+    @Query("select a from Account a where a.username like :username and a.active = true ")
     Account findAccountByUsername(String username);
     //TODO include accId in the query
 //    @Query("select a from Account a where a.id = :accountId")
