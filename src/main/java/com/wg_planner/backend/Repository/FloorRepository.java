0f18ef0c774037f6d5f1e61fa3a10564c2a4e995
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-27 14:14:24 +0200

    active check FloorRepository

--- src/main/java/com/wg_planner/backend/Repository/FloorRepository.java
+++ src/main/java/com/wg_planner/backend/Repository/FloorRepository.java
@@ -13,33 +13,34 @@ public interface FloorRepository extends JpaRepository<Floor, Long> {
     @Query("select r from Room r " +
             "where r.floor.id = :floorIdToSearch and r.active = true ")
     List<Room> findAllRoomsInFloor(@Param("floorIdToSearch") Long floorIdToSearch);
+
     @Query("select r from Room r " +
             "where r.floor.id = :floorIdToSearch and r.occupied = false and r.active = true ")
     List<Room> findAllNonOccupiedRoomsInFloor(@Param("floorIdToSearch") Long floorIdToSearch);
+
     @Query("select r from Room r " +
             "where r.floor.id = :floorIdToSearch and r.occupied = true and r.residentAccount.away = false and r.active = true ")
     List<Room> findAllOccupiedAndResidentNotAwayRoomsInFloor(@Param("floorIdToSearch") Long floorIdToSearch);
 
-    @Query("select f.tasks from Floor f " +
-            "where f.id = :floorIdToSearch having ")
+    @Query("select t from Task t where t.floor.id = :floorIdToSearch and t.active = true")
     List<Task> findAllTasksInFloor(@Param("floorIdToSearch") Long floorIdToSearch);
 
     @Query("select r from Room r " +
-            "where r.floor = :floorIdToSearch and r.residentAccount.away = false ")
+            "where r.floor = :floorIdToSearch and r.residentAccount.away = false and r.active = true")
     List<Room> findAllAvailableRooms(@Param("floorIdToSearch") Long floorIdToSearch);
 
-    @Query("select f from Floor f where f.floorName = :floorNumber")
+    @Query("select f from Floor f where f.floorName = :floorNumber and f.active = true ")
     Floor findFloorByNumber(@Param("floorNumber") String floorNumber);
 
-    @Query("select f from Floor f where f.id = :floorId")
+    @Query("select f from Floor f where f.id = :floorId and f.active = true ")
     Floor findFloorById(@Param("floorId") long floorId);
 
-    @Query("select f from Floor f ")
+    @Query("select f from Floor f where f.active = true ")
     List<Floor> findAllFloors();
 
-    @Query("select floorCode from Floor")
+    @Query("select f.floorCode from Floor f where f.active = true")
     List<String> findAllFloorCodes();
 
-    @Query("select f from Floor f where f.floorCode = :floorCodeToSearch")
+    @Query("select f from Floor f where f.floorCode = :floorCodeToSearch and f.active = true")
     Floor findFloorByFloorCode(@Param("floorCodeToSearch") String floorCodeToSearch);
 }
