030cad995f6170d55be19b87bec32b56de5a9087
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-27 14:15:35 +0200

    active check RoomRepository

--- src/main/java/com/wg_planner/backend/Repository/RoomRepository.java
+++ src/main/java/com/wg_planner/backend/Repository/RoomRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("select r from Room r " +
-            "where lower( r.roomName ) like lower(concat( '%', :roomNumberToSearch, '%' )) and r.floor.id = :floorId")
+            "where lower( r.roomName ) like lower(concat( '%', :roomNumberToSearch, '%' )) and r.floor.id = :floorId and r.active = true ")
     Room findRoomByNumber(@Param("roomNumberToSearch") String roomNumberToSearch, @Param("floorId") Long floorId);
 
-    @Query("select r from Room r where r.residentAccount.id = :residentId ")
+    @Query("select r from Room r where r.residentAccount.id = :residentId and r.active = true and r.residentAccount.active = true ")
     Room findRoomByResidentId(Long residentId);
 
-    @Query("select r from Room r where r.id = :roomId ")
+    @Query("select r from Room r where r.id = :roomId and r.active = true ")
     Room findRoomByRoomId(Long roomId);
 }
