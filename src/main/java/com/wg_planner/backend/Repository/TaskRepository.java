3aa2f477165450306edb2651a0946a36c2ca1141
d.paul <s8dipaul@stud.uni-saarland.de>
2021-06-27 14:16:09 +0200

    active check TaskRepository

--- src/main/java/com/wg_planner/backend/Repository/TaskRepository.java
+++ src/main/java/com/wg_planner/backend/Repository/TaskRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    @Query("select t from Task t where t.id = :taskId ")
+    @Query("select t from Task t where t.id = :taskId and t.active = true ")
     Task findTaskByTaskId(Long taskId);
 }
