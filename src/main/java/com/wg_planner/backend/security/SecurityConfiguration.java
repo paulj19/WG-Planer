50f556d75357214d39ce06ae5df045fe8ed01965
d.paul <s8dipaul@stud.uni-saarland.de>
2021-09-22 14:29:07 +0200

    feat: remember-me, seems to be working but not acid tested

--- src/main/java/com/wg_planner/backend/security/SecurityConfiguration.java
+++ src/main/java/com/wg_planner/backend/security/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.wg_planner.backend.security;
 
+import com.wg_planner.backend.Service.AccountDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,9 +23,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String REGISTER_FORM = "/register_form";
     private static final String CREATE_FLOOR_URL = "/create_floor";
     private static final String OVERVIEW_URL = "/overview";
+    @Autowired
+    AccountDetailsService accountDetailsService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception     {
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .requestCache().requestCache(new CustomRequestCache())
                 .and().authorizeRequests()
@@ -36,7 +41,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage(OVERVIEW_URL).permitAll()
                 .loginProcessingUrl(LOGIN_PROCESSING_URL)
                 .failureUrl(LOGIN_FAILURE_URL)
-                .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL);
+                .and().logout().logoutSuccessUrl(LOGOUT_SUCCESS_URL)
+                .and()
+                .rememberMe()
+//                .rememberMeCookieName("wg-planer")
+                .userDetailsService(this.userDetailsService())
+//                .useSecureCookie(true)
+                .alwaysRemember(true)
+                .tokenValiditySeconds(2419200);//28 days
     }
 
     @Bean
@@ -59,5 +71,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 "/images/**",
                 "/styles/**",
                 "/h2-console/**");
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return accountDetailsService;
     }
 }
